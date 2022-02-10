8c321a9d827c6098687efc03eeae42103739e311
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-11 00:51:59 +0600

    emergency banner added

---
+++ src/Components/EmergencyServices/EmergencyBanner/EmergencyBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmergencyBanner = () => {
+    return (
+        <div>
+            <h1>this is emergency banner</h1>
+        </div>
+    );
+};
+
+export default EmergencyBanner;
--- src/Components/Services/OurServices/OurServics.js
+++ src/Components/Services/OurServices/OurServics.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Appointment from '../Appointment/Appointment';
+import ServiceBanner from '../ServiceBanner/ServiceBanner';
 import Services from '../Services/Services';
 import Testimonials from '../Testimonials/Testimonials';
 
 const h = () => {
     return (
         <div>
+            <ServiceBanner></ServiceBanner>
             <Services></Services>
+            <Appointment></Appointment>
             <Testimonials></Testimonials>
         </div>
     );
