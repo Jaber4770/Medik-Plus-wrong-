2c8c6d1af8ed8d6ccc1111e9ef0f11def5e273b1
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-10 23:53:39 +0600

    phermacy compoent added on aboutus

---
+++ src/Components/AboutUs/Phermacy/Phermacy.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Phermacy = () => {
+    return (
+        <div>
+            <h1>get all medicine here free of cost</h1>
+        </div>
+    );
+};
+
+export default Phermacy;
