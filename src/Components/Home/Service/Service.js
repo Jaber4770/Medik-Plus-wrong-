65fae5dfe0f93b8f5b97568ec32f679f41b8fdc0
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-10 23:40:27 +0600

    services component displayed

--- src/Components/Home/Service/Service.js
+++ src/Components/Home/Service/Service.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Service = () => {
     return (
         <div>
-            
+            <h1>this is service component</h1>
         </div>
     );
 };
--- src/Components/Home/Services/Services.js
+++ src/Components/Home/Services/Services.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Services = () => {
     return (
         <div>
-            
+            <h1>this is services component</h1>
         </div>
     );
 };
