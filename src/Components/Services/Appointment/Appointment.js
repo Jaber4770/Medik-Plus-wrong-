3d049b10ee693e8f426a6adc753c3f550f96ed54
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-11 00:27:21 +0600

    appointment added on services

---
+++ src/Components/Services/Appointment/Appointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appointment = () => {
+    return (
+        <div>
+            <h1>take your appointment with our specialized Doctors</h1>
+        </div>
+    );
+};
+
+export default Appointment;
--- src/Components/Services/OurServices/OurServics.js
+++ src/Components/Services/OurServices/OurServics.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Services from '../Services/Services';
+import Testimonials from '../Testimonials/Testimonials';
 
 const h = () => {
     return (
         <div>
             <Services></Services>
+            <Testimonials></Testimonials>
         </div>
     );
 };
