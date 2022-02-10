75a9d56fe85275764649bcdf667bfc86439c93a0
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-11 02:00:57 +0600

    all preflight disabled by tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
+import AboutUs from './Components/AboutUs/AboutUs/AboutUs';
+import AppointmentForm from './Components/Appointment/AppointmentForm';
+import ContactUs from './Components/ContactUs/ContactUs/ContactUs';
+import EmergencyServices from './Components/EmergencyServices/EmergencyServices/EmergencyServices';
 import Home from './Components/Home/Home/Home';
+import Services from './Components/Home/Services/Services';
+import Login from './Components/Login/Login/Login';
 import Footer from './Components/Shared/Footer/Footer';
 import Header from './Components/Shared/Header/Header';
 
@@ -8,6 +14,12 @@ function App() {
     <div className="App">
       <Header></Header>
       <Home></Home>
+      <Services></Services>
+      <AppointmentForm></AppointmentForm>
+      <ContactUs></ContactUs>
+      <EmergencyServices></EmergencyServices>
+      <AboutUs></AboutUs>
+      <Login></Login>
       <Footer></Footer>
     </div>
   );
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            
+            <h1>now i used react tailwind css</h1>
         </div>
     );
 };
