7a6927571385a0ff3e14682305a66eeaa99fb8e2
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-10 23:42:15 +0600

    specialized doctors component displayed in home component

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -3,11 +3,13 @@ import Banner from '../Banner/Banner';
 import Contact from '../Contact/Contact';
 import TipAndNews from '../RecentTipAndNews/TipAndNews';
 import Services from '../Services/Services';
+import SpecializedDoctors from '../SpecializedDoctors/SpecializedDoctors';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <SpecializedDoctors></SpecializedDoctors>
             <Services></Services>
             <Contact></Contact>
             <TipAndNews></TipAndNews>
--- src/Components/Home/SpecializedDoctors/SpecializedDoctors.js
+++ src/Components/Home/SpecializedDoctors/SpecializedDoctors.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SpecializedDoctors = () => {
     return (
         <div>
-            
+            <h1>this is Specialized Doctors component</h1>
         </div>
     );
 };
