de5e48a136d0040bd0c4cb04724e8276f6a64951
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-11 00:17:09 +0600

    ourservices added

---
+++ src/Components/Services/OurServices/OurServics.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Services from '../Services/Services';
+
+const h = () => {
+    return (
+        <div>
+            <Services></Services>
+        </div>
+    );
+};
+
+export default h;
