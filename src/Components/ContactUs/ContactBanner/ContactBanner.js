9e5c96120b09f7b2a7e3fc7350ec63b2def157c6
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-11 00:54:47 +0600

    contact banner added

---
+++ src/Components/ContactUs/ContactBanner/ContactBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactBanner = () => {
+    return (
+        <div>
+            <h1>this is contact banner</h1>
+        </div>
+    );
+};
+
+export default ContactBanner;
--- src/Components/EmergencyServices/EmergencyServices/EmergencyServices.js
+++ src/Components/EmergencyServices/EmergencyServices/EmergencyServices.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Ambulance from '../AmbulanceServices/Ambulance';
+import EmergencyBanner from '../EmergencyBanner/EmergencyBanner';
 import Helicopter from '../HelicopterServices/Helicopter';
 
 const EmergencyServices = () => {
     return (
         <div>
+            <EmergencyBanner></EmergencyBanner>
             <Ambulance></Ambulance>
             <Helicopter></Helicopter>
         </div>
