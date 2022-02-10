5c136ac2838a985e6aec8c1745f562d9e3d9cab4
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-10 23:35:06 +0600

    all home component created

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import Contact from '../Contact/Contact';
+import TipAndNews from '../RecentTipAndNews/TipAndNews';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
-            <h1>this is home</h1>
+            <Banner></Banner>
+            <Services></Services>
+            <Contact></Contact>
+            <TipAndNews></TipAndNews>
         </div>
     );
 };
