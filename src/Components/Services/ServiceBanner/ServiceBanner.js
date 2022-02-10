5fab3e6562a4b3f6421f3172b7f8dd35c907ab2d
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-11 00:29:03 +0600

    service banner added on services page

---
+++ src/Components/Services/ServiceBanner/ServiceBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServiceBanner = () => {
+    return (
+        <div>
+            <h1>this is servicesBanner</h1>
+        </div>
+    );
+};
+
+export default ServiceBanner;
