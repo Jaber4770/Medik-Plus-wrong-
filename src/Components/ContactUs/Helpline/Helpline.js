5b30a452bdb11828087b7a91d96725808488441b
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-10 23:57:42 +0600

    helpline component added

---
+++ src/Components/ContactUs/Helpline/Helpline.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Helpline = () => {
+    return (
+        <div>
+            <h1>24/7 helpline open for heath tips and get services</h1>
+        </div>
+    );
+};
+
+export default Helpline;
