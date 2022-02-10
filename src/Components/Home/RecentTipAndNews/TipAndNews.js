5208e5c1c07aa244efd56d2d3f73d9247d084b9e
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-10 23:32:14 +0600

    all folder created and all home component created

---
+++ src/Components/Home/RecentTipAndNews/TipAndNews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TipAndNews = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TipAndNews;
---
+++ src/Components/Home/SpecializedDoctors/SpecializedDoctors.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SpecializedDoctors = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SpecializedDoctors;
