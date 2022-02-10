8034c49e688d7c0e79148e251a247f3723419fc1
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-10 23:43:41 +0600

    header component displayed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './Components/Home/Home/Home';
+import Header from './Components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Home></Home>
     </div>
   );
