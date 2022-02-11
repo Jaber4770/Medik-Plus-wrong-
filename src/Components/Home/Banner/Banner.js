5e53ade192e7ebda5de9871e94ca6a7f1dc1cfd7
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-11 20:59:39 +0600

    i am failed to make the site with tailwind css

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,24 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import BannerImg from './BannerImg';
-
+import React from 'react';
 
 const Banner = () => {
-    const [img, setImg] = useState([]);
-
-    useEffect(() => {
-        fetch('/HomeBannerImg.js')
-            .then(res => res.json())
-            .then(data => setImg(data))
-    }, [])
-
     return (
-        <div className='container mx-auto md:container md:mx-auto'>
-            {
-                img.map(img => <BannerImg
-                key={img.id}
-                imgs={img}
-                ></BannerImg>)
-            }
+        <div>
+            <h1>banner</h1>
         </div>
     );
 };
